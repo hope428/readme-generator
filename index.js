9391cd199ebdcec96e9b547208a356d872ac6eab
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-02 18:13:50 -0500

    created rough markdown in generate markdown function. added inquirer questions and generateMarkdown call

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Describe your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the usage information?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What are the testing instructions?",
+    name: "testing",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => generateMarkdown);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +14,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage 
+${data.usage}
+
+## Credits
+${data.credts}
+
+## License
+${data.license}
+
+## How to Contribute
+${data.contribution}
+
+## Tests
+${data.testing}
 `;
 }
 
